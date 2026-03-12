b0c9a43371dc0954e1d5fcd55062b3b9ed24bfdb
creadorodrigo <creado.rodrigo@gmail.com>
2026-03-12 13:58:50 -0300

    Add files via upload

---
+++ src/components/RelatoriosIA.tsx
@@ -0,0 +1,9 @@
+// src/pages/RelatoriosIA.tsx
+// Wrapper page para o módulo de Relatórios IA
+// Rota: /relatorios-ia (admin only)
+
+import RelatoriosIA from '../components/RelatoriosIA'
+
+export default function RelatoriosIAPage() {
+  return <RelatoriosIA />
+}
